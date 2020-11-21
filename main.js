3466e2ced0cc77ee67639e6a63ba1a88dc21fcc4
cchura94 <cchura.cpc@gmail.com>
2020-11-20 20:10:40 -0400

    segundo

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ __webpack_require__.r(__webpack_exports__);
 // The list of file replacements can be found in `angular.json`.
 const environment = {
     production: false,
-    servidor: 'http://edtics.herokuapp.com/api',
+    servidor: '//edtics.herokuapp.com/api',
 };
 /*
  * For easier debugging in development mode, you can import the following file
