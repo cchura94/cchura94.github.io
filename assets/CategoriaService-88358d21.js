77dd4a2d2ed939e90bd381794e33dc4956bb1c02
cristian <cchura.cpc@gmail.com>
2023-07-18 23:13:45 -0400

    PAGE VUE

--- assets/CategoriaService-88358d21.js
+++
@@ -1,1 +0,0 @@
-import{k as t}from"./index-e5a80d16.js";const i={listar(){return t().get("/categoria")},guardar:r=>t().post("/categoria",r),mostrar:function(r){return t().get(`/categoria/${r}`)},modificar:(r,a)=>t().put(`/categoria/${a}`,r),eliminar:r=>t().delete(`/categoria/${r}`)};export{i as c};
